eb94d4d517feaa2def098b4747c31daa3f68a958
viii21 <111612552+viii21@users.noreply.github.com>
2022-09-11 17:44:48 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  //click on an image, get a message
+
+  $("#dogeOne").click(function () {
+    $("#outputDiv").hide();
+
+    console.log("user clicked dogeOne 2");
+    $("#outputMsg").text("You Lose, you Stupid Loser");
+    $("#outputDiv").toggle();
+  });
+
+  $("#dogeTwo").click(function () {
+    $("#outputDiv").hide();
+    console.log("user clicked dogeTwo 2");
+    $("#outputMsg").text("you has WOW");
+    $("#outputDiv").toggle();
+  });
+});
